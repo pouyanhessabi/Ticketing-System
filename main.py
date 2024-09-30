308d20c716441354b0f32090388ed15a8d917ba2
pouyan.hessabi <pouyan.hessabi@gmail.com>
2024-09-30 16:39:20 +0330

    Fix Ticket page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 # from .model import Ticket
@@ -22,14 +22,25 @@ def profile():
 
 
 @main.route('/ticket')
-@login_required
 def ticket():
     return render_template('ticket.html')
+
     # return render_template('profile.html', name=current_user.name, id=current_user.id, phone=current_user.phone,
     #                        address=current_user.address, creation_date=current_user.creation_date)
-    # ticket_from_ui = request.form.getlist('ticket[]')
+
     # ticket = Ticket()
     # try:
     #     add_ticket(ticket)
     # except Exception as err:
     #     print(err)
+
+
+@main.route('/ticket', methods=['POST'])
+def ticket2():
+    description = request.form.getlist('description')
+    issue_type = request.form.getlist('type')
+    from .model import Ticket
+    new_ticket = Ticket()
+
+    print(description, issue_type)
+    return render_template('show_ticket.html', description=description[0], type=issue_type[0])
